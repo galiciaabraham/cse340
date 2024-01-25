da44d2f545c0db96fd0e5d786806f7696bbea479
galiciaabraham <galicia.abraham@hotmail.com>
2024-01-25 09:13:24 -0600

    added middleware to inv routes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
+const middleware = require("../utilities")
 
 //Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-router.get("/detail/:invId", invController.buildDetailsPage);
+router.get("/type/:classificationId", middleware.handleErrors(invController.buildByClassificationId));
+router.get("/detail/:invId", middleware.handleErrors(invController.buildDetailsPage));
 
 module.exports = router;
